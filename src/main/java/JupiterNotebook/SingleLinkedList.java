3805d2ca86c6e916d66fd4a2dd73b09bca124ac2
Choi Sung Sik <css9596@naver.com>
2021-08-07 23:23:57 +0900

    LinkedList Node 추가, 삭제 구현
    일일 느낀점
    LinkedList 삭제 시 연결된 Node가 없으면 java의 garbage Collection이 삭제를 해주는데, 이런 부분 까지 생각을 하면서 개발을 해야 하는 것을 느꼈다. 또, Node를 삭제 하면서 LinkedList를 연결 하는 것 까지 구현을 해야 하는것에 자동으로 이런 기능을 하는게 있는지 궁금해졌고, 찾아봐야겠다.

--- src/main/java/JupiterNotebook/SingleLinkedList.java
+++ src/main/java/JupiterNotebook/SingleLinkedList.java
@@ -13,6 +13,43 @@ public class SingleLinkedList<T>{
         }
     }
 
+    public static void main(String[] args){
+        SingleLinkedList<Integer> test = new SingleLinkedList<Integer>();
+
+        test.addNode(1);
+        System.out.println(test.head.data); // 1
+
+        test.addNode(2);
+        System.out.println(test.head.next); // 추가된 2의 값을 가지고 있는 노드의 주소
+        // ex) JupiterNotebook.SingleLinkedList$Node@15db9742
+        System.out.println(test.head.next.data);    // 1의 값을 가지고 있는 노드의 다음 노드의 값. //2
+        test.addNode(3);
+        test.addNode(4);
+        test.addNode(5);
+        test.addNode(6);
+
+        test.addNodeInside(10, 3);   //10이라는 Node를 3 데이터를 가진 Node에 입력
+        test.addNodeInside(20, 20);  //20이라는 Node를 20 데이터를 가진 Node에 입력, 없으면 맨 뒤 Node에 생성.
+
+        test.printAll();    //전체 리스트 출력.
+
+        // Node삭제 TEST
+        test.delNode(10);
+        test.printAll();    //전체 리스트 출력.
+
+        // HEAD를 삭제 했을 때
+        test.delNode(1);
+        test.printAll();    //전체 리스트 출력.
+
+        // 마지막 노드 삭제
+        test.delNode(20);
+        test.printAll();    //전체 리스트 출력.
+
+        // 없는 노드 삭제시
+        System.out.println(test.delNode(30));
+
+    }//-END main();
+
     public void addNode(T data){
         if(head == null)
             head = new Node<T>(data);
@@ -25,25 +62,6 @@ public class SingleLinkedList<T>{
         }
     }//END addNode
 
-    public static void main(String[] args){
-        SingleLinkedList<Integer> test = new SingleLinkedList<Integer>();
-
-        test.addNode(1);
-        System.out.println(test.head.data); // 1
-
-        test.addNode(2);
-        System.out.println(test.head.next); // 추가된 2의 값을 가지고 있는 노드의 주소
-                                            // ex) JupiterNotebook.SingleLinkedList$Node@15db9742
-        System.out.println(test.head.next.data);    // 1의 값을 가지고 있는 노드의 다음 노드의 값. //2
-        test.addNode(3);
-        test.addNode(4);
-        test.addNode(5);
-        test.addNode(6);
-
-        test.printAll();
-
-    }//-END main();
-
     public void printAll(){
         if(head!=null){
             Node<T> node = this.head;
@@ -51,6 +69,58 @@ public class SingleLinkedList<T>{
             while (node.next != null){
                 node = node.next;
                 System.out.println(node.data);
+            }
+        }
+    }
+
+    public Node<T> search(T data){
+        if(this.head == null){
+            return null;
+        }else{
+            Node<T> node = this.head;
+            while(node != null){
+                if(node.data == data){
+                    return node;
+                }else{
+                    node = node.next;
+                }
+            }
+        }
+        return null;
+    }
+
+    public void addNodeInside(T data, T isData){
+        Node<T> searchNode = this.search(isData);
+
+        if(searchNode == null){
+            this.addNode(data);
+        }else{
+            Node<T> nextNode = searchNode.next;
+            searchNode.next = new Node<T>(data);
+            searchNode.next.next = nextNode;
+        }
+    }
+
+
+    public boolean delNode(T isData){
+        if(this.head == null){
+            //데이터 미존재.
+            return false;
+        } else {
+            Node<T> node = this.head;
+            if(node.data == isData)/*입력한 값이 노드에 존재를 하면 */{
+                // 첫번째 Node가 head 일 때
+                this.head = this.head.next;
+                return true;
+            } else /*head가 아니면*/{
+                while(node.next != null)/*다음 노드값을 가리키고 있으면.*/{
+                    if(node.next.data == isData){
+                        node.next = node.next.next;
+                        return true;
+                    }
+                    node = node.next;
+                }
+                return false;
             }
         }
     }
