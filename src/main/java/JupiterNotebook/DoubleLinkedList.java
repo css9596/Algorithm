b7db817d81aa4aadd36fb2e107993e196c5883bc
Choi Sung Sik <css9596@naver.com>
2021-08-09 22:42:19 +0900

    DouleLinkedList 중간에 데이터 추가 구현
    일일 느낀점
    DoubleLinkedList에 중간부분 추가 구현을 통해서 느낀점은 복잡한 코드일 수록 쪼개서 하나하나 그림을 그리고 생각을 하면서 짜야 한다는 것을 배웠다.

--- src/main/java/JupiterNotebook/DoubleLinkedList.java
+++ src/main/java/JupiterNotebook/DoubleLinkedList.java
@@ -27,6 +27,10 @@ public class DoubleLinkedList<T> {
         System.out.println(test.searchFromHead(3));
         System.out.println(test.searchFromTail(3));
 
+        System.out.println(test.insertToFront(3, 10));
+
+        test.printAll();
+
 
     }
 
@@ -86,6 +90,34 @@ public class DoubleLinkedList<T> {
             }
             //head까지 순회 했을 때 데이터 미존재. == 찾으려는 데이터가 없음.
             return null;
+        }
+    }
+
+    public boolean insertToFront(T existedData, T addData){
+        if(this.head == null){
+            //추가하려고 할 때 Head가 존재하지 않으면. => 데이터 가 없으므로 넣어준다.
+            this.head = new Node<T>(addData);
+            this.tail = this.head;
+            return  true;
+        }else if(existedData == this.head.data)/*현재 추가하려는 데이티가 이미 존재 한다면.*/{
+            Node<T> newHead = new Node<T>(addData);
+            newHead.next = this.head;
+            this.head = newHead;
+            return true;
+        }else{
+            Node<T> node = this.head;
+            while (node != null){
+                if(node.data == existedData){
+                    Node<T> nodePrev = node.prev;
+
+                    nodePrev.next = new Node<T>(addData);
+                    nodePrev.next.next = node;
+                    return true;
+                }else{
+                    node = node.next;
+                }
+            }
+            return false;
         }
     }
 
