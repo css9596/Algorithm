3f4aaf96dbdd8d37a27c1b8128e0198425029b75
Choi Sung Sik <css9596@naver.com>
2021-08-10 22:15:02 +0900

    HashTable에 대해 공부했다.
    일일 느낀점
    hasFunc: Division기법을 이용하여 저장할 슬롯을 지정하는데, 키값의 첫번째 인자를 Char형으로 변경하여 사용을 하다보니 중복되는 문자열이 들어갈 경우 값이 제일 마지막으로 변경이 되어 처리가 된다... 다른 방법이 있는지 고민해봐야겠다.

---
+++ src/main/java/JupiterNotebook/MyHash.java
@@ -0,0 +1,53 @@
+package JupiterNotebook;
+
+public class MyHash {
+    public Slot[] hashTable;
+
+    public MyHash(Integer size) {
+        this.hashTable = new Slot[size];
+    }
+
+    public class Slot {
+        String value;
+        Slot(String value) {
+            this.value = value;
+        }
+    }
+
+    public int hashFunc(String key) {
+        return (int)(key.charAt(0)) % this.hashTable.length;
+    }
+
+    public boolean saveData(String key, String value) {
+        Integer address = this.hashFunc(key);
+        if (this.hashTable[address] != null) {
+            this.hashTable[address].value = value;
+        } else {
+            this.hashTable[address] = new Slot(value);
+        }
+        return true;
+    }
+
+    public String getData(String key) {
+        Integer address = this.hashFunc(key);
+        System.out.println(address);
+        if (this.hashTable[address] != null) {
+            return this.hashTable[address].value;
+        } else {
+            return null;
+        }
+    }
+
+    public static void main(String[] args){
+        MyHash test = new MyHash(20);
+
+        test.saveData("SungSikAge", "27");
+        test.saveData("Ah-Ha", "165");
+        test.saveData("Ok", "Choi");
+
+
+        System.out.println(test.getData("Ok"));
+    }
+
+
+}
