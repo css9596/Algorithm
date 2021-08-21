be26a6228b86731501bb76c34dd83253d7d3ab70
Choi Sung Sik <css9596@naver.com>
2021-08-22 01:19:17 +0900

    깊이 우선 탐색 DFS 공부
    HashMap으로 그래프를 구현,
    ArrayList로 그래프의 Level을 표현
    
    BFS(너비우선탐색)
    BFS에서는 두개의Queue를 이용하여 구현을 했지만 DFS는 하나의 Stack, Queue를 이용하여 구현을 했다.

---
+++ src/main/java/JupiterNotebook/DepthFirstSearch.java
@@ -0,0 +1,59 @@
+package JupiterNotebook;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class DepthFirstSearch {
+    /**
+     * DepthFirstSearch(깊이우선탐색)
+     *  한 노드의 자식을 타고 끝까지 순회한 후 다시 돌아와서 다른 형제들의 자식을 타고 내려가며 순회함
+     * */
+    public static void main(String[] args) {
+        DepthFirstSearch test = new DepthFirstSearch();
+        DFSSearch test2 = new DFSSearch();
+
+        HashMap<String, ArrayList<String>> dataList = new HashMap<>();
+        dataList.put("A", new ArrayList<String>(Arrays.asList("B","C")));
+        dataList.put("B", new ArrayList<String>(Arrays.asList("A","D")));
+        dataList.put("C", new ArrayList<String>(Arrays.asList("A","G","H","I")));
+        dataList.put("D", new ArrayList<String>(Arrays.asList("B","E","F")));
+        dataList.put("E", new ArrayList<String>(Arrays.asList("D")));
+        dataList.put("F", new ArrayList<String>(Arrays.asList("D")));
+        dataList.put("G", new ArrayList<String>(Arrays.asList("C")));
+        dataList.put("H", new ArrayList<String>(Arrays.asList("C")));
+        dataList.put("I", new ArrayList<String>(Arrays.asList("C","J")));
+        dataList.put("J", new ArrayList<String>(Arrays.asList("I")));
+
+        System.out.println(test2.dfsFunc(dataList, "A"));
+
+        //test.DFSTest();
+    }
+
+    public void DFSTest(){
+        ArrayList<String> aList = new ArrayList<String>();
+        aList.add("A");
+        aList.add("B");
+        String node = aList.remove(aList.size() - 1); //Stack POP()
+        System.out.println(aList);
+        System.out.println(node);
+    }
+
+    public static class DFSSearch{
+        public ArrayList<String> dfsFunc(HashMap<String, ArrayList<String>>graph, String startNode){
+            ArrayList<String> visited = new ArrayList<>();
+            ArrayList<String> needVisit = new ArrayList<>();
+
+            needVisit.add(startNode);
+
+            while (needVisit.size() > 0)/*방문해야 할 노드가 존재하면*/{
+                String node= needVisit.remove(needVisit.size() - 1);
+                if(!visited.contains(node))/*해당 노드가 방문한적이 없다면*/{
+                    visited.add(node);  /*방문*/
+                    needVisit.addAll(graph.get(node)); /*해당 노드가 가지고 있는 자식들 추가.*/
+                }
+            }
+            return visited;
+        }
+    }
+}
